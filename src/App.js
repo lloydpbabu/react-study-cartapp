7948ec9ec2f852429849f95c0bea9f1d26a3f0a4
Lloyd P Babu <lloydpbabu@gmail.com>
2021-06-08 01:27:59 +0530

    Added Modal and modal open / close function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from  './Components/Cart/Cart';
 
 function App() {
+
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setcartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setcartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
